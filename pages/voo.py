5b2d6cf52021d9d5009ab8aed096e7d534cb522b
Salis_Silva <salissilvadev@gmail.com>
2025-11-11 12:33:38 -0300

    validacao duracao voo

--- pages/voo.py
+++ pages/voo.py
@@ -3,10 +3,20 @@ from services.admin.admin_voo_service import *
 
 def cadastrar_voo():
     print("Area de Cadastro do voo")
-    id_aviao = int(input("digite o modelo do voo: "))
+    id_aviao = int(input("digite o id do aviao: "))
     local_partida = input("digite o local de partida: ").capitalize()
     local_destino = input("digite o local de destino: ").capitalize()
-    duracao = input("digite a duracao do voo: ")
+    while True:
+        try:
+            duracao_horas = int(input("Digite a duração do voo (em horas): "))
+            duracao_min = int(input("Digite a duração do voo (em minutos): "))
+            if duracao_horas < 0 or duracao_min < 0 or duracao_min >= 60:
+                print("Valores inválidos! Minutos devem estar entre 0 e 59")
+                continue
+            duracao = f"{duracao_horas:02}:{duracao_min:02}:00"
+            break
+        except ValueError:
+            print("Digite apenas números inteiros para horas e minutos.")
     cadastrar_voo_service(id_aviao, local_partida, local_destino,duracao)
 
 def listar_voo():
